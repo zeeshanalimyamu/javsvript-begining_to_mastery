cdbc8b78e0b9b018370cfdd4434e58a1ac5439bf
Zeeshan <zeeshanalimyamu@gmail.com>
2023-02-01 15:06:16 +0530

    undefines_null

---
+++ 11.js
@@ -0,0 +1,21 @@
+// undefined 
+// null
+
+// let firstName;
+// console.log(typeof firstName);
+// firstName = "Zeeshan";
+// console.log(typeof firstName, firstName);
+
+// let myVariable = null;
+// console.log(myVariable);
+// myVariable = "Zeeshan";
+// console.log(myVariable, typeof myVariable);
+// console.log(typeof null);
+// bug , error 
+
+// BigInt
+// let myNumber = BigInt(12);
+// let sameMyNumber = 123n;
+// // console.log(myNumber);
+// // console.log(Number.MAX_SAFE_INTEGER);
+// console.log(myNumber+ sameMyNumber);
