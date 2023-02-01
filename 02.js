0a985c3c43e5fc4699b6397434f5a494f923d044
Zeeshan <zeeshanalimyamu@gmail.com>
2023-02-01 14:51:33 +0530

    How to print name

---
+++ 02.js
@@ -0,0 +1,19 @@
+"use strict";
+// intro to variables
+
+// variables can store some information
+// we can use that information later
+// we can change that information later
+
+// declare a variable 
+
+var firstName = "zeeshan";
+
+// use a variable 
+console.log(firstName);
+
+// change value 
+
+firstName = "azizul";
+
+console.log(firstName);
