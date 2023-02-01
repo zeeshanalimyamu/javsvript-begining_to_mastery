cedf752077ecb75f79acb45146a886d3c79af377
Zeeshan <zeeshanalimyamu@gmail.com>
2023-02-01 14:57:54 +0530

    string indexing

---
+++ 05.js
@@ -0,0 +1,16 @@
+// String indexing 
+
+let firstName = "zeeshandfjakldsfdf";
+
+//  z    e   e   s   h   a   n 
+//  0    1   2   3   4   5   6
+
+// console.log(firstName[0]);
+// length of string 
+// firstName.length 
+
+console.log(firstName.length);
+
+console.log(firstName[firstName.length-2]);
+
+// last Index : length - 1 
